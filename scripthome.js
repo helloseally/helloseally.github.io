4e40f659568f41a7a65865d66f175339d5de7dfd
MahimaKale <91439024+MahimaKale@users.noreply.github.com>
2022-04-16 20:39:38 -0700

    resptry

--- scripthome.js
+++ scripthome.js
@@ -29,3 +29,54 @@ for (var i=0; i<bubbles.length; i++){
     specificDiv.style.top = Num +"px";
     specificDiv.style.left = NumL +"px";
 }
+
+ // https://stackoverflow.com/questions/13784686/moving-an-image-randomly-around-a-page
+
+    $(document).ready(function(){
+        AnimateDiv($('.embub'));
+        AnimateDiv($('.embub1'));
+        AnimateDiv($('.embub2'));
+        AnimateDiv($('.embub3'));
+        AnimateDiv($('.embub4'));
+        AnimateDiv($('.embub5'));
+        AnimateDiv($('.embub6'));
+        AnimateDiv($('.embub7'));
+    });
+
+    function makeNewPosition($container) {
+        var h = $container.height() + 500;
+        var w = $container.width() + Math.random();
+
+        var nh = Math.floor(Math.random() * h);
+        var nw = Math.floor(Math.random() * w);
+
+        return [nh, nw];
+    };
+
+    function AnimateDiv($target){
+        var newq = makeNewPosition($target.parent());
+        var oldq = $target.offset();
+        var speed = calcSpeed([oldq.top, oldq.left], newq);
+
+        $target.animate({
+            top: newq[0],
+            left: newq[1]
+        }, speed, function() {
+            AnimateDiv($target);
+        });
+    };
+
+    function calcSpeed(prev, next) {
+
+        var x = Math.abs(prev[1] - next[1]);
+        var y = Math.abs(prev[0] - next[0]);
+
+        var greatest = x > y ? x : y;
+
+        var speedModifier = 0.02;
+
+        var speed = Math.ceil(greatest / speedModifier);
+
+        return speed;
+
+    }
