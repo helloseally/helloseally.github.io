2d6d38942d5e6d5fc8067cf13860fea9b130fe8d
Julia Ma <julma@ucdavis.edu>
2022-04-16 17:01:10 -0700

    text box beginning

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-
-//granim: 
-var granimInstance = new Granim({
-    element: '#canvas-basic',
-    direction: 'left-right',
-    transitionSpeed: 10000,
-    isPausedWhenNotInView: true,
-    states : {
-        "default-state": {
-            gradients: [
-                ['#C5C4E5', '#8EA5D2'],
-                ['#8EA5D2', '#5A709B'],
-                ['#5A709B', '#C5C4E5']
-            ]
+(function() {
+    //granim: 
+    var granimInstance = new Granim({
+        element: '#canvas-basic',
+        direction: 'left-right',
+        transitionSpeed: 10000,
+        isPausedWhenNotInView: true,
+        states : {
+            "default-state": {
+                gradients: [
+                    ['#C5C4E5', '#8EA5D2'],
+                    ['#8EA5D2', '#5A709B'],
+                    ['#5A709B', '#C5C4E5']
+                ]
+            }
         }
-    }
-});
+    });
+
+    var writeForm = document.getElementById("write-form");
+    var message = document.getElementById("message");
+
+    message.addEventListener("keyup", function(event) {
+        event.preventDefault();
+
+        var curLen = message.value.length;
+        var charsLeft = 280 - curLen;
+        var count = document.getElementById("char-count");
+        count.innerHTML = charsLeft;
+
+        if (message.scrollHeight > message.clientHeight) {
+            message.style.height = message.scrollHeight+"px";
+        }
+    });
+
+    writeForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        // go somewhere or do something?
+
+        
+    });
+})();
